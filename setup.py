05e8e50fa56c67f71e8780ec6f35fd06734bff4c
Nirmit Kasodariya <nirmit1311@gmail.com>
2025-07-07 14:42:59 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='Nirmit',
+    author_email='nirmit1311@gmail.com',
+    packages=find_packages(),
+    install_requires=['pandas','numpy','seaborn']
+)
